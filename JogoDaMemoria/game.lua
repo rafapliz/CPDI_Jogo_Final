887a13b8e95c9288b6ebc64dca1f2eb1fb90a82e
rafapliz <39142461+rafapliz@users.noreply.github.com>
2023-08-10 22:26:42 -0300

    Adicionado cenas e botões no menu

--- JogoDaMemoria/game.lua
+++ JogoDaMemoria/game.lua
@@ -1,149 +1,185 @@
--- local composer = require("composer")
--- local scene = composer.newScene()
+display.setStatusBar (display.HiddenStatusBar)
+local composer = require("composer")
+local scene = composer.newScene()
 
--- local bg = display.newImageRect("imagens/bg.jpg")
--- bg.x = display.contentCenterX
--- bg.y = display.contentCenterY
--- bg:setFillColor(0,0,1)
+local function gotoMenu()
+	composer.gotoScene ("menu")
+end
 
--- local function shuffle(t)
---    local n = #t
---    while n > 2 do
---       local k = math.random(n)
---       t[n], t[k] = t[k], t[n]
---       n = n - 1
---    end
---    return t
--- end
+local function gotoRecordes ()
+	composer.gotoScene ("recordes")
+end
 
--- function scene:create(event)
---    local sceneGroup = self.view
+function scene:create(event)
+   local sceneGroup = self.view
 
---    local cards = {"ODS2"}
+   function scene:create( event ) 
 
---    cards = shuffle(cards)
+      local sceneGroup = self.view
+      -- Code here runs when the scene is first created but has not yet appeared on screen
 
---    local function cardTapped(event)
---       local card = event.target
---       if not card.isFlipped then
---          transition.to(card, {time = 300, xScale = 0.1, onComplete = function()
---             card:setFillColor(1, 1, 1)
---             transition.to(card, {time = 300, xScale = 1})
---          end})
---          card.isFlipped = true
---       end
---    end
-
---    local xOffset = 60
---    local yOffset = 100
-
---    for i = 1, #cards do
-	
--- 	local cardFront = display.newImage(sceneGroup, "imagens/"..cards[i]..".jpg", xOffset, yOffset)
--- 	local cardBack = display.newImage(sceneGroup, "imagens/"..cards[i].."-oculta.jpg", xOffset, yOffset)
-     
---       cardBack.isFlipped = true
-
---       cardFront:addEventListener("tap", cardTapped)
       
---       xOffset = xOffset + 80
---       if i % 4 == 0 then
---          xOffset = 60
---          yOffset = yOffset + 120
---       end
---    end
--- end
-
--- scene:addEventListener("create", scene)
-
--- return scene
-
-
-
-local cardImages = {
-    "imagens/ODS1.jpg",
-    "imagens/ODS2.jpg",
-    "imagens/ODS3.jpg",
-    "imagens/ODS4.jpg",
-    "imagens/ODS5.jpg",
-    "imagens/ODS6.jpg",
-    "imagens/ODS7.jpg",
-    "imagens/ODS8.png",
-    "imagens/ODS9.png",
-    "imagens/ODS10.png"
-}
-
-local boardSize = 4
-local totalPairs = (boardSize * boardSize) / 2
-local cardSize = 100
-local cardSpacing = 10
-local startX = (display.contentWidth - (boardSize * (cardSize + cardSpacing)) + cardSpacing) / 2
-local startY = (display.contentHeight - (boardSize * cardSize)) / 2
-
-local revealedCards = {}
-local pontos = 0
-
-local function shuffleCards()
-    for i = 1, #cardImages do
-        local randIndex = math.random(i, #cardImages)
-        cardImages[i], cardImages[randIndex] = cardImages[randIndex], cardImages[i]
-    end
+     end
 end
 
-local function flipCard(event)
-    local card = event.target
 
-    if card.isFlipped or #revealedCards >= 2 then
-        return
-    end
+function scene:show( event ) -- Imediatamente antes ou depois da cena
 
-    card.isFlipped = true
-    card.rect.isVisible = false
+	local sceneGroup = self.view
+	local phase = event.phase
 
-    table.insert(revealedCards, card)
-
-    if #revealedCards == 2 then
-        local card1, card2 = unpack(revealedCards)
-
-        if card1.cardImage == card2.cardImage then
-            pontos = pontos + 10
-        else
-            pontos = pontos - 5
-            timer.performWithDelay(1000, function()
-                card1.isFlipped = false
-                card2.isFlipped = false
-                card1.rect.isVisible = true
-                card2.rect.isVisible = true
-            end)
-        end
-
-        revealedCards = {}
-    end
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is still off screen (but is about to come on screen)
+        local bg = display.newImageRect ("imagens/bg.jpg", 612, 408)
+        bg.x = display.contentCenterX
+        bg.y = display.contentCenterY 
+        
+              local cardImages = {
+                "imagens/1.jpg",
+                "imagens/2.jpg",
+                "imagens/3.jpg",
+                "imagens/4.jpg",
+                "imagens/5.jpg",
+                "imagens/6.jpg",
+                "imagens/7.jpg",
+                "imagens/8.png",
+                "imagens/9.png",
+                "imagens/10.png",
+                 "imagens/ODS1OBJ.png",
+                 "imagens/ODS2OBJ.png",
+                 "imagens/ODS3OBJ.png",
+                 "imagens/ODS4OBJ.png",
+                 "imagens/ODS5OBJ.png",
+                 "imagens/ODS6OBJ.png",
+                 "imagens/ODS7OBJ.png",
+                 "imagens/ODS8OBJ.png",
+                 "imagens/ODS9OBJ.png",
+                 "imagens/ODS10OBJ.png",
+                 
+                }
+            
+            local numRows = 4
+            local numCols = 5
+            local totalPairs = (numRows * numCols) / 2
+            local cardSize = 75
+            local cardSpacing = 4
+            local startX = (display.contentWidth - (4 * (cardSize + cardSpacing)) + cardSpacing) / 1
+            local startY = (display.contentHeight - (3.53 * (cardSize + cardSpacing)))
+                
+            local revealedCards = {}
+            local pontos = 0
+            
+            local function shuffleCards()
+                for i = 1, #cardImages do
+                    local randIndex = math.random(i, #cardImages)
+                    cardImages[i], cardImages[randIndex] = cardImages[randIndex], cardImages[i]
+                end
+            end
+            
+            local function flipCard(event)
+                local card = event.target
+            
+                if card.isFlipped or #revealedCards >= 2 then
+                    return
+                end
+            
+                card.isFlipped = true
+                card.rect.isVisible = false
+                card.cardImage.isVisible = true
+                
+                table.insert(revealedCards, card)	
+                print("Carta 1 ",  card1)
+                print("Carta 2 ",  card2)
+            
+                if #revealedCards == 2 then
+                    local card1, card2 = unpack(revealedCards)
+                    local image1 = card1.cardImage.filename
+                    local image2 = card2.cardImage.filename
+                    print(image1)
+                    print(image2)
+                    if image1 == image2 then
+                        pontos = pontos + 10
+                    else
+                        pontos = pontos - 5
+                        timer.performWithDelay(1000, function()
+                            card1.isFlipped = false
+                            card2.isFlipped = false
+                            card1.rect.isVisible = true
+                            card2.rect.isVisible = true
+                            card1.cardImage.isVisible = false
+                            card2.cardImage.isVisible = false
+                        end)
+                    end
+            
+                    revealedCards = {}
+                end
+            end
+            
+            --shuffleCards()
+            
+            for row = 1, numRows do
+                for col = 1, numCols do
+                    local index = (row - 1) * numCols + col
+                    local cardX = startX + (col - 1) * (cardSize + cardSpacing)
+                    local cardY = startY + (row - 1) * (cardSize + cardSpacing)
+                    
+                    local card = display.newRect(cardX, cardY, cardSize, cardSize)
+                    card:setFillColor(1, 1, 0)
+                    card:addEventListener("tap", flipCard)
+                    card.rect = display.newRect(cardX, cardY, cardSize, cardSize)
+                    card.rect:setFillColor(0.6,0.8,0)
+                    card.rect.isVisible = true
+                    card.isFlipped = false
+            
+                    local cardImage = display.newImageRect(cardImages[index], cardSize - 10, cardSize - 10)
+                    cardImage.x = cardX
+                    cardImage.y = cardY
+                    cardImage.isVisible = false
+                    card.cardImage = cardImage
+                end
+            end
+            
+            local pontosText = display.newText({
+                text = "Pontos: " .. pontos,
+                x = 30,
+                y = display.contentHeight - 100,
+                fontSize = 20
+            })
+            pontosText:setFillColor(1, 1, 1)
+            
+	elseif ( phase == "did" ) then
+		-- Code here runs when the scene is entirely on screen
+	end
 end
 
-shuffleCards()
+function scene:hide( event ) -- Imediatamente antes ou depois que a cena sair da tela
 
-for row = 1, boardSize do
-    for col = 1, boardSize do
-        local index = (row - 1) * boardSize + col
-        local card = display.newRect(startX + (col - 1) * (cardSize + cardSpacing), startY + (row - 1) * cardSize, cardSize, cardSize)
-        card:setFillColor(1, 0, 0)
-        card.rect = display.newRect(card.x, card.y, card.width, card.height)
-        card.rect:setFillColor(1, 0, 0)
-        card.rect.isVisible = true
-        card.cardImage = cardImages[index]
-        card.isFlipped = false
+	local sceneGroup = self.view
+	local phase = event.phase
 
-        local cardImage = display.newImageRect(cardImages[index], cardSize - 10, cardSize - 10)
-        cardImage.x, cardImage.y = card.x, card.y
-        card:addEventListener("tap", flipCard)
-    end
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is on screen (but is about to go off screen)
+
+	elseif ( phase == "did" ) then
+		-- Code here runs immediately after the scene goes entirely off screen
+      
+	end
 end
 
-local pontosText = display.newText({
-    text = "Pontos: " .. pontos,
-    x = display.contentCenterX,
-    y = display.contentHeight - 20,
-    fontSize = 20
-})
-pontosText:setFillColor(0, 0, 0)
+
+
+function scene:destroy( event )
+
+	local sceneGroup = self.view
+	
+    
+end
+
+
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+-- -----------------------------------------------------------------------------------
+
+return scene
--- JogoDaMemoria/menu.lua
+++ JogoDaMemoria/menu.lua
@@ -1,18 +1,80 @@
 local composer = require("composer")
+local scpriptGame  = require("game")
 local scene = composer.newScene()
+
+local function gotoGame()
+	composer.gotoScene ("game")
+end
+
+local function gotoRecordes ()
+	composer.gotoScene ("recordes")
+end
 
 function scene:create(event)
    local sceneGroup = self.view
 
-   local playButton = display.newText(sceneGroup, "Jogar", display.contentCenterX, display.contentCenterY, native.systemFont, 40)
+   function scene:create( event ) 
 
-   local function goToGame()
-      composer.gotoScene("game")
+      
    end
-
-   playButton:addEventListener("tap", goToGame)
 end
 
-scene:addEventListener("create", scene)
+function scene:show( event ) -- Imediatamente antes ou depois da cena
+
+	local sceneGroup = self.view
+	local phase = event.phase
+
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is still off screen (but is about to come on screen)
+		local sceneGroup = self.view
+		-- Code here runs when the scene is first created but has not yet appeared on screen
+		local bg2 = display.newImageRect (sceneGroup, "imagens/bg2.png",1024/1.4, 534/1.4 	)
+		bg2.x = display.contentCenterX
+		bg2.y = display.contentCenterY
+	
+	 
+		local play = display.newImageRect (sceneGroup, "imagens/play.png", 506/3, 185/3, native.systemFont, 44)
+		play.x, play.y = 470, 150
+		
+	 
+		local recordes = display.newImageRect (sceneGroup, "imagens/recordes.png", 510/3, 239/3, native.systemFont, 44)
+		recordes.x, recordes.y = 472, 215
+	 
+		play:addEventListener("tap", gotoGame)
+		recordes:addEventListener("tap", gotoRecordes)
+	elseif ( phase == "did" ) then
+		-- Code here runs when the scene is entirely on screen
+	end
+end
+
+function scene:hide( event ) -- Imediatamente antes ou depois que a cena sair da tela
+
+	local sceneGroup = self.view
+	local phase = event.phase
+
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is on screen (but is about to go off screen)
+
+	elseif ( phase == "did" ) then
+		-- Code here runs immediately after the scene goes entirely off screen
+      
+	end
+end
+
+
+
+function scene:destroy( event )
+
+	local sceneGroup = self.view
+	
+    
+end
+
+
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+-- -----------------------------------------------------------------------------------
 
 return scene
