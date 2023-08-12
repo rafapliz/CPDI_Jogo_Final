7a2b492c69b3889ac45326d18ccd91eb08061860
rafapliz <39142461+rafapliz@users.noreply.github.com>
2023-08-11 22:38:40 -0300

    Adicionado imagens nas ODSs e imagem dos pontos
    Ainda não consegui fazer funcionar a função de comparação.

--- JogoDaMemoria/game.lua
+++ JogoDaMemoria/game.lua
@@ -1,6 +1,8 @@
-display.setStatusBar (display.HiddenStatusBar)
-local composer = require("composer")
-local scene = composer.newScene()
+display.setStatusBar (display.HiddenStatusBar) -- Remove a barra de Status
+local composer = require("composer") -- Chama o composer para cenas
+local scene = composer.newScene() -- cria a cena
+
+
 
 local function gotoMenu()
 	composer.gotoScene ("menu")
@@ -30,10 +32,25 @@ function scene:show( event ) -- Imediatamente antes ou depois da cena
 
 	if ( phase == "will" ) then
 		-- Code here runs when the scene is still off screen (but is about to come on screen)
+        local cornerRadius = 8
+
         local bg = display.newImageRect ("imagens/bg.jpg", 612, 408)
         bg.x = display.contentCenterX
         bg.y = display.contentCenterY 
-        
+
+            -- Configurações iniciais
+            local numRows = 4 -- Número de linhas de cartas
+            local numCols = 5 -- Número de colunas de cartas
+            local cardSize = 75 -- Tamanho das cartas
+            local cardSpacing = 4 -- Espaçamento entre as cartas
+            local startX = (display.contentWidth - (4 * (cardSize + cardSpacing)) + cardSpacing) / 1
+            local startY = (display.contentHeight - (3.53 * (cardSize + cardSpacing)))
+            local cornerRadius = 8 -- Variável para arredondamento das bordas   
+            local revealedCards = {} -- Armazena as cartas viradas
+            
+
+            local pontos = 0 -- Pontuação do jogador
+        -- Imagem das cartas
               local cardImages = {
                 "imagens/1.jpg",
                 "imagens/2.jpg",
@@ -45,107 +62,152 @@ function scene:show( event ) -- Imediatamente antes ou depois da cena
                 "imagens/8.png",
                 "imagens/9.png",
                 "imagens/10.png",
-                 "imagens/ODS1OBJ.png",
-                 "imagens/ODS2OBJ.png",
-                 "imagens/ODS3OBJ.png",
-                 "imagens/ODS4OBJ.png",
-                 "imagens/ODS5OBJ.png",
-                 "imagens/ODS6OBJ.png",
-                 "imagens/ODS7OBJ.png",
-                 "imagens/ODS8OBJ.png",
-                 "imagens/ODS9OBJ.png",
-                 "imagens/ODS10OBJ.png",
+                "imagens/1.jpg",
+                "imagens/2.jpg",
+                "imagens/3.jpg",
+                "imagens/4.jpg",
+                "imagens/5.jpg",
+                "imagens/6.jpg",
+                "imagens/7.jpg",
+                "imagens/8.png",
+                "imagens/9.png",
+                "imagens/10.png",
+                --  "imagens/ODS1OBJ.png",
+                --  "imagens/ODS2OBJ.png",
+                --  "imagens/ODS3OBJ.png",
+                --  "imagens/ODS4OBJ.png",
+                --  "imagens/ODS5OBJ.png",
+                --  "imagens/ODS6OBJ.png",
+                --  "imagens/ODS7OBJ.png",
+                --  "imagens/ODS8OBJ.png",
+                --  "imagens/ODS9OBJ.png",
+                --  "imagens/ODS10OBJ.png",
                  
                 }
-            
-            local numRows = 4
-            local numCols = 5
-            local totalPairs = (numRows * numCols) / 2
-            local cardSize = 75
-            local cardSpacing = 4
-            local startX = (display.contentWidth - (4 * (cardSize + cardSpacing)) + cardSpacing) / 1
-            local startY = (display.contentHeight - (3.53 * (cardSize + cardSpacing)))
-                
-            local revealedCards = {}
-            local pontos = 0
-            
+                -- for i = 1, #cardImages do
+                --     print("Posição " .. i .. ": " .. cardImages[i])
+                -- end
+
+
+  -- Criação das cartas
+ 
+-- Função para comparar as cartas viradas
+local function compareCards()
+    local card1 = revealedCards[1]
+    local card2 = revealedCards[2]
+
+    if card1.cardImage == card2.cardImage then
+        pontos = pontos + 10
+        card1:removeEventListener("tap", flipCard)
+        card2:removeEventListener("tap", flipCard)
+    else
+        pontos = pontos - 1
+        timer.performWithDelay(1000, function()
+            card1.isFlipped = false
+            card2.isFlipped = false
+            card1.rect.isVisible = true
+            card2.rect.isVisible = true
+            card1.cardImage.isVisible = true
+            card2.cardImage.isVisible = true
+            card1.frontImage.isVisible = false
+            card2.frontImage.isVisible = false
+        end)
+    end
+
+    revealedCards = {}  -- Limpa as cartas viradas
+end
+
+ -- Função para virar a carta
+ local function flipCard(event)
+    
+    local card = event.target
+    
+    if card.isFlipped then
+         return
+     end                
+    
+    card.isFlipped = true
+    card.isVisible = false  -- Esconde a imagem de fundo
+    card.frontImage.isVisible = true   -- Mostra a imagem frontal
+
+    table.insert(revealedCards, card)
+--   for i = 1, #revealedCards do
+--       print(revealedCards[i])
+--  end
+    if #revealedCards == 2 then
+        local card1 = revealedCards[1]
+        local card2 = revealedCards[2]
+        
+            if card1 == card2 then
+                pontos = pontos + 10
+                card1:removeEventListener("tap", flipCard)
+                card2:removeEventListener("tap", flipCard)
+            else
+                pontos = pontos - 1
+                timer.performWithDelay(1000, function()
+                    card1.isFlipped = false
+                    card2.isFlipped = false
+                    --card1.rect.isVisible = true
+                   -- card2.rect.isVisible = true
+                    card1.isVisible = true
+                    card2.isVisible = true
+                    card1.frontImage.isVisible = false
+                    card2.frontImage.isVisible = false
+                end)
+            end
+        
+              
+
+        revealedCards = {}
+        
+    end
+end
+
+for row = 1, numRows do
+    for col = 1, numCols do
+        local index = (row - 1) * numCols + col
+        local cardX = startX + (col - 1) * (cardSize + cardSpacing)
+        local cardY = startY + (row - 1) * (cardSize + cardSpacing)
+        
+        -- Cria o retângulo arredondado com a imagem de fundo
+
+        local card = display.newImageRect("imagens/bg-carta.png", cardSize+20, cardSize+5)  -- Imagem de fundo da carta
+        card.x = cardX 
+        card.y = cardY 
+        card.isVisible = true  -- Mostra a imagem de fundo inicialmente
+        card:addEventListener("tap", flipCard)
+                            
+        local frontImage = display.newImageRect(cardImages[index], cardSize - 10, cardSize - 10)  -- Imagem frontal da carta
+        frontImage.x = cardX
+        frontImage.y = cardY
+        frontImage.isVisible = false
+        card.frontImage = frontImage
+
+       
+    end
+end      
+
+            -- Função para embaralhar as cartas a cada reinício
             local function shuffleCards()
                 for i = 1, #cardImages do
                     local randIndex = math.random(i, #cardImages)
                     cardImages[i], cardImages[randIndex] = cardImages[randIndex], cardImages[i]
                 end
-            end
-            
-            local function flipCard(event)
-                local card = event.target
-            
-                if card.isFlipped or #revealedCards >= 2 then
-                    return
-                end
-            
-                card.isFlipped = true
-                card.rect.isVisible = false
-                card.cardImage.isVisible = true
+            end  
+                --shuffleCards()
+
                 
-                table.insert(revealedCards, card)	
-                print("Carta 1 ",  card1)
-                print("Carta 2 ",  card2)
-            
-                if #revealedCards == 2 then
-                    local card1, card2 = unpack(revealedCards)
-                    local image1 = card1.cardImage.filename
-                    local image2 = card2.cardImage.filename
-                    print(image1)
-                    print(image2)
-                    if image1 == image2 then
-                        pontos = pontos + 10
-                    else
-                        pontos = pontos - 5
-                        timer.performWithDelay(1000, function()
-                            card1.isFlipped = false
-                            card2.isFlipped = false
-                            card1.rect.isVisible = true
-                            card2.rect.isVisible = true
-                            card1.cardImage.isVisible = false
-                            card2.cardImage.isVisible = false
-                        end)
-                    end
-            
-                    revealedCards = {}
-                end
-            end
-            
-            --shuffleCards()
-            
-            for row = 1, numRows do
-                for col = 1, numCols do
-                    local index = (row - 1) * numCols + col
-                    local cardX = startX + (col - 1) * (cardSize + cardSpacing)
-                    local cardY = startY + (row - 1) * (cardSize + cardSpacing)
-                    
-                    local card = display.newRect(cardX, cardY, cardSize, cardSize)
-                    card:setFillColor(1, 1, 0)
-                    card:addEventListener("tap", flipCard)
-                    card.rect = display.newRect(cardX, cardY, cardSize, cardSize)
-                    card.rect:setFillColor(0.6,0.8,0)
-                    card.rect.isVisible = true
-                    card.isFlipped = false
-            
-                    local cardImage = display.newImageRect(cardImages[index], cardSize - 10, cardSize - 10)
-                    cardImage.x = cardX
-                    cardImage.y = cardY
-                    cardImage.isVisible = false
-                    card.cardImage = cardImage
-                end
-            end
-            
+
+            local imagemPontos = display.newImageRect("imagens/pontos.png", 590/3.5, 223/3.5)
+            imagemPontos.x, imagemPontos.y = 30, display.contentHeight - 100
+
             local pontosText = display.newText({
-                text = "Pontos: " .. pontos,
-                x = 30,
-                y = display.contentHeight - 100,
-                fontSize = 20
+                text =  pontos,
+                x = 85,
+                y = display.contentHeight - 102,
+                fontSize = 25
             })
-            pontosText:setFillColor(1, 1, 1)
+            pontosText:setFillColor(0, 0, 0)
             
 	elseif ( phase == "did" ) then
 		-- Code here runs when the scene is entirely on screen
